0c5cd1feed408a2addc05dd038c37122d09ae77e
nannuke <88435869+nannuke@users.noreply.github.com>
2022-08-09 22:52:46 +0800

    Add files via upload

---
+++ project1/src/mypackage/Hello.java
@@ -0,0 +1,35 @@
+package mypackage;
+
+public class Hello {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
